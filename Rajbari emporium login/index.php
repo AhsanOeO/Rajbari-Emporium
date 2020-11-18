9f49f9e0e89b96c3f5d54b51e06318523f9698c0
Ahsan Zaman <74678638+AhsanOeO@users.noreply.github.com>
2020-11-19 01:18:49 +0600

    NEW

---
+++ Rajbari emporium login/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <title>Rajbari Emporium </title>
+ <link rel="stylesheet" href="/css/bootstrap.min.css">
+ <link rel="stylesheet" href="/css/custom.css">
+ <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+ <script src="/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+  <div class="container-fluid">
+   <p>hhhh</p>
+  <nav class="navbar navbar-float-top" role="navigation">
+      <ul class="navbar navbar-nav">
+        <a href="index.php" class="navbar-brand">Rajbari Emporium</a>
+        <li class="dropdown">
+        	<a href="#" class="dropdown-toggle" data-toggle="dropdown">Men<span class="caret"></span></a>
+        </li>
+
+      </ul>
+  </nav>
+</div>
+</body>
+</html>
